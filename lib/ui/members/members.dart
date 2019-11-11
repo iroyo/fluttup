676ad123172ff0f2a769848b7d9e2d85807159e9
Isaac Royo <iroyo@doonamis.es>
2019-11-11 17:49:19 +0100

    added topbar

--- lib/ui/members/members.dart
+++ lib/ui/members/members.dart
@@ -13,30 +13,29 @@ class Members extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        brightness: Brightness.light,
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Center(
-          child: Text(
-            "Fluttup",
-            style: TextStyle(
-              color: colorPrimary,
-              fontSize: 28,
-              fontFamily: "Forte",
+          brightness: Brightness.light,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Center(
+            child: Text(
+              "Fluttup",
+              style: TextStyle(
+                color: colorPrimary,
+                fontSize: 28,
+                fontFamily: "Forte",
+              ),
             ),
           ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              FluttupIcons.filters,
-              color: colorAccent,
-            ),
-            onPressed: null,
-          )
-        ],
-        bottom: TopNavigationBar(),
-      ),
+          actions: [
+            IconButton(
+              icon: Icon(
+                FluttupIcons.filters,
+                color: colorAccent,
+              ),
+              onPressed: null,
+            )
+          ],
+          bottom: TopNavigationBar()),
       body: GridView.count(
           padding: EdgeInsets.all(12),
           mainAxisSpacing: 8,
@@ -49,13 +48,55 @@ class Members extends StatelessWidget {
 }
 
 class TopNavigationBar extends StatelessWidget implements PreferredSizeWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(decoration: BoxDecoration(color: Colors.yellow),);
+    return Container(
+      height: 42,
+      child:  Padding(
+        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            RawMaterialButton(
+              onPressed: null,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 16),
+                child: Text(
+                  "All",
+                  style: TextStyle(
+                    color: colorPrimary,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+            RawMaterialButton(
+              onPressed: null,
+              fillColor: colorAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(18.0),
+                side: BorderSide(color: Colors.red),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 16),
+                child: Text(
+                  "In common",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+
+    );
   }
 
   @override
   Size get preferredSize => Size.fromHeight(42.0);
-
 }
